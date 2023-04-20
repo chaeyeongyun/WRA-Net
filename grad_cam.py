fcd6a45c83aca063e84c506a27f3806a5ceaa95e
chaeyeongyun <70565663+chaeyeongyun@users.noreply.github.com>
2023-04-20 11:58:31 +0900

    Update grad_cam.py

--- grad_cam.py
+++ grad_cam.py
@@ -11,6 +11,8 @@ from PIL import Image
 import torchvision.transforms.functional as TF
 import matplotlib.pyplot as plt
 from tqdm import tqdm
+from threading import Event
+import argparse
 
 def min_max_norm(input):
     if isinstance(input, np.ndarray):
@@ -29,23 +31,19 @@ def interpolate(input, size:tuple):
 class GradCAM():
     def __init__(self, model:torch.nn.Module, target_layers:List[nn.Module], device:torch.device):
         self.model , self.target_layers = model, target_layers
-        # if device == '-1':
-        #     self.device = torch.device('cpu')
-        # else:
-        #     if torch.cuda.is_available(): 
-        #         self.device = torch.device('cuda:'+device)
-        #     else: raise Exception('this device is not available') 
         self.device = device
         self.gradients, self.activations = [], []
+        self.handles = []
         for target_layer in target_layers:
-            target_layer.register_forward_hook(self.save_activation_hook)
-            target_layer.register_forward_hook(self.save_gradient_hook)
+            self.handles += [target_layer.register_forward_hook(self.save_activation_hook)]
+            self.handles += [target_layer.register_forward_hook(self.save_gradient_hook)]
+
     
     def __call__(self, input_tensor:torch.Tensor, targets:List[nn.Module]):
         input_tensor.to(self.device)
         input_tensor.requires_grad_(True)
         outputs = self.model(input_tensor)
-
+        
         self.model.zero_grad()
         loss = sum([target(output) for target, output in zip(targets, outputs)])
         loss.backward(retain_graph=True)
@@ -61,45 +59,47 @@ class GradCAM():
             target_layer = self.target_layers[i]
             layer_activations = activations_list[i]
             layer_grads = grads_list[i]
-            # cam_weights = np.mean(layer_grads, axis=(2, 3))
             cam_weights = torch.mean(layer_grads, dim=(2, 3))
             weighted_activations = cam_weights[:, :, None, None] * layer_activations # add axis and multiply
-            # cam = weighted_activations.sum(axis=1) # (1, h, w)
             cam = torch.sum(weighted_activations, dim=1) # (1, h, w)
-            # cam = np.maximum(cam, 0) # ReLU
             cam = torch.maximum(cam, torch.zeros_like(cam)) # ReLU
             cam = min_max_norm(cam)
-            # cam = np.transpose(cam, (1, 2, 0)) # h, w, 1
-            # cam = np.expand_dims(cv2.resize(cam, target_size), 0) # h, w -> 1, h, w
             cam = interpolate(cam, size=target_size)
             cam_per_target_layer += [cam[:, None, :]]
-        # cam_per_target_layer = np.concatenate(cam_per_target_layer, axis=0)
+        
         cam_per_target_layer = torch.cat(cam_per_target_layer, dim=0)
-        # cam_per_target_layer = np.maximum(cam_per_target_layer, 0)
         cam_per_target_layer = torch.maximum(cam_per_target_layer, torch.zeros_like(cam_per_target_layer))
-        # final_cam = np.mean(cam_per_target_layer, axis=1)
+        
         final_cam = torch.mean(cam_per_target_layer, dim=1)
         final_cam = min_max_norm(final_cam)
-        # final_cam = cv2.resize(np.transpose(final_cam, (1, 2, 0)), target_size)
+        
         final_cam = interpolate(final_cam, size=target_size)
         final_cam = torch.squeeze(final_cam)
         final_cam = final_cam.cpu().numpy()
+        self.release()
         return final_cam
         
         
     def save_activation_hook(self, m, x, y):
         activation = y
-        self.activations += [activation.cpu().detach()]
+        self.activations += [activation.detach()]
     
     def save_gradient_hook(self, m, x, y):
        assert hasattr(y, "requires_grad"), 'y must have ''requires_grad'' attribute'
        
        def _store_grad_hook(grad):
-           self.gradients = [grad.cpu().detach()] + self.gradients
+           self.gradients = [grad.detach()] + self.gradients
        
        y.register_hook(_store_grad_hook)
+       
+    def __del__(self):
+        self.release()
     
+    def release(self):
+        for handle in self.handles:
+            handle.remove()
     
+
 
 class GradCAMTarget:
     def __init__(self, class_num, mask:torch.Tensor):
@@ -118,7 +118,7 @@ def main(class_num, img_dir, model, model_weights_path, target_layers:List, devi
             device = torch.device('cuda:'+device)
         else: raise Exception('this device is not available')
     model.load_state_dict(torch.load(model_weights_path)['network'])
-    
+    #### error model이 갑자기 unet객체가 아니게됨 띠용? 어디가 문제인지 확인하기
     model = model.to(device)
     print('model weights are loaded')
     def loop(class_num, imgs):
@@ -138,10 +138,13 @@ def main(class_num, img_dir, model, model_weights_path, target_layers:List, devi
             result_img = cam_on_img(img, gray_cam, img_weight=0.4, device=device)
             filename, extend = os.path.splitext(filename)[:]
             plt.imsave(os.path.join(save_path, filename+f'_cls{class_num}'+extend), cv2.resize(result_img, (orgw//2, orgh//2)))
+            del input_tensor
+            del cam
     imgs = glob(os.path.join(img_dir, '*.png'))
     if isinstance(class_num, list):
         for i in class_num:
             loop(i, imgs)
+            Event().wait(10)
     else:
         loop(class_num, imgs)
     
@@ -170,9 +173,8 @@ def cam_on_img(img:np.ndarray, gray_cam:np.ndarray,img_weight:float, device:torc
 if __name__ == "__main__":
     model = models.Unet()
     target_layers = [model.expansive_path.conv4]
-
-    main(class_num=[0, 1, 2], 
-        img_dir='/content/data/cropweed_total/CWFID/seg/val/input', 
+    main(class_num=1, 
+        img_dir='/content/data/cropweed_total/CWFID/seg/train/input', 
         model=model, 
         model_weights_path='/content/drive/MyDrive/segtrain/CWFID/Unet-ep50-0/ckpoints/best_miou.pth', 
         target_layers=target_layers, 
